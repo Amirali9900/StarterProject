b2f5f17e2b3ab8794fb90292f1dbcfd42af71649
aa.eyshi <aa.eyshi@HASANZADEH-PC>
2025-05-05 16:36:34 +0330

    init

---
+++ StarterProject/StarterProject/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarterProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult get()
+        {
+            //get all data from user table
+
+
+
+            return Ok(/*return your data in here*/);
+        }
+
+        [HttpPost]
+        public IActionResult post(/*get you viewmodel*/)
+        {
+            //create a data in user table
+
+
+
+            return Ok(/*return the created data*/);
+        }
+    }
+}
